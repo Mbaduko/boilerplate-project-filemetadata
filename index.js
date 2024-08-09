e4652b4a2204a9fd065e8fd4a67f2d9f392469c8
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-08-09 10:13:15 +0200

    feat: Done

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer')
 require('dotenv').config()
 
-var app = express();
+const  app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+const storage = multer.diskStorage({});
+const upload = multer({storage});
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", upload.single('upfile'),(req, res) => {
+  const {originalname:name, size, mimetype:type} = req.file;
+  return res.json({name, type, size});
+})
 
 
 const port = process.env.PORT || 3000;
